7ab74b2ea8692233e75fc0998941fe950f427489
gentaliana <gentaliana@gmail.com>
2020-07-06 20:56:56 +0400

    test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string;
+  text: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-test';
+  // title = 'app-test';
+  togle = true;
+  cards: Card[] = [
+    { title: 'Card 1', text: 'This is Card 1' },
+    { title: 'Card 2', text: 'This is Card 2' },
+    { title: 'Last Card', text: 'This is Card 3' }
+  ];
+
+  toggleCards(){
+    this.togle = !this.togle;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeRu from '@angular/common/locales/ru'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
 
+registerLocaleData(localeRu, 'ru');
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CardComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
